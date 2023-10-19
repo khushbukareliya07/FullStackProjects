d5860cb7b96198c2c1c105b2c47e85c22db7f9e1
khushbukareliya07 <khushbu.karelia@gmail.com>
2023-10-19 18:26:56 -0500

    Phase1: Added Models and tables for Domain Models of Blog

---
+++ Bloggie/Bloggie.Web/Data/BlogieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BlogieDbContext : DbContext
+    {
+        public BlogieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> Blogposts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
