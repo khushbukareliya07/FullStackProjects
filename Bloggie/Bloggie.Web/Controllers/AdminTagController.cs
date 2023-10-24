95f050cd4a970c9d48cbfce114ef816fbc966028
khushbukareliya07 <khushbu.karelia@gmail.com>
2023-10-24 17:13:59 -0500

    build: added show all tag functionality, added database connection and saving data to DB using Constructor Injection

--- Bloggie/Bloggie.Web/Controllers/AdminTagController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminTagController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagController : Controller
     {
+        private readonly BlogieDbContext _blogieDbContext;//this will help us connect to the DB and perform read/write operations based on CRUD!
+
+        public AdminTagController(BlogieDbContext blogieDbContext) 
+        {
+            _blogieDbContext = blogieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //mapping addtagRequest to the Tag Domain Model
+            var tag = new Tag() //saving data to the tags table!
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+            //_blogieDbContext.Tags.Add(new Models.Domain.Tag() { Name = addTagRequest.Name, DisplayName = addTagRequest.DisplayName });
+
+            _blogieDbContext.Tags.Add(tag);
+            _blogieDbContext.SaveChanges();
+
+            return RedirectToAction("ListofTags");
+        }
+
+        [HttpGet]
+        [ActionName("ListofTags")]
+        public IActionResult ListofTags() {
+
+            //use _DBcontext to read the Tags data from table!
+            var listofTags = _blogieDbContext.Tags.ToList();
+            return View(listofTags);
         }
     }
 }
